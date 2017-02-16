93a8c74034aaa969dc3de0f6a7619b009ee3acfd
Osman Perviz <osman.perviz@evopark.de>
2017-02-16 21:01:17 +0100

    Init

---
+++ index.android.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReportApp from './app'
+import { AppRegistry } from 'react-native';
+
+
+AppRegistry.registerComponent('ReportApp', () => ReportApp);
---
+++ index.ios.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { AppRegistry } from 'react-native';
+import Main from './app/index'
+
+
+
+
+AppRegistry.registerComponent('ReportApp', () => Main);
